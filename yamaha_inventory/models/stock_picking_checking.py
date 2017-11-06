d4a9f21afd3e9d406a2762da4ad943d6716e0775
Matija <matija.pildek@gmail.com>
2017-11-06 12:44:11 +0100

    dodane funkvcionalnosti

--- yamaha_inventory/models/stock_picking_checking.py
+++ yamaha_inventory/models/stock_picking_checking.py
@@ -3,26 +3,36 @@
 from odoo import api, fields, models, tools, _
 
 class StockPickingChecking(models.Model):
-	_name = 'stock.picking.checking'
+	_name = 'stock.picking.checking1'
 
-	partner_id = fields.Many2one('res.partner', 'Partner')
-	product_id = fields.Many2one('product.product', 'Product')
-	product_qty = fields.Float('ToDo')
+	partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+	spc_line = fields.One2many('stock.picking.checking.line1', 'spc_line_id', string='Line ids')
+
+class StockPickingCheckingLine(models.Model):
+	_name = 'stock.picking.checking.line1'
+
+	spc_line_id = fields.Many2one('stock.picking.checking1', string='Order id', ondelete='cascade', readonly=True) #inverzna relacija
+	partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+	product_id = fields.Many2one('product.product', 'Product', readonly=True)
+	product_qty = fields.Float('ToDo', readonly=True)
 	qty_done = fields.Float('Done')
-	name = fields.Char('Reference',)
-	location_dest_id = fields.Many2one('stock.location', "Destination Location Zone")
-	origin = fields.Char('Source Document')
-	state = fields.Char('State')
-	sp_id = fields.Integer('SP_id')
-	spo_id = fields.Integer('SPO_id')
-	picking_id = fields.Integer('Picking_id')
+	name = fields.Char('Reference', readonly=True)
+	location_dest_id = fields.Many2one('stock.location', "Destination Location Zone", readonly=True)
+	origin = fields.Char('Source Document', readonly=True)
+	state = fields.Char('State', readonly=True)
+	sp_id = fields.Integer('SP_id', readonly=True)
+	spo_id = fields.Integer('SPO_id', readonly=True)
+	picking_id = fields.Integer('Picking_id', readonly=True)
+
+
 
 class StockPickingType(models.Model):
 	_inherit = 'stock.picking.type'
 
 	@api.multi
 	def get_data(self):
-		self.env.cr.execute("""delete from stock_picking_checking""")
+		self.env.cr.execute("""delete from stock_picking_checking_line1""")
+		self.env.cr.execute("""delete from stock_picking_checking1""")
 		self.env.cr.execute("""
 			select  sp.id as sp_id,
 			        spo.id as spo_id,
@@ -40,19 +50,67 @@ class StockPickingType(models.Model):
 				where state like 'assigned'
 				order by sp.partner_id""")
 		res = self.env.cr.dictfetchall()
-		SPC_obj = self.env['stock.picking.checking']
-		for r in res:
-			SPC_obj.create({'product_id': r['product_id'], 'product_qty':r['product_qty'],
-			                'qty_done': r['qty_done'], 'name':r['name'],
-			                'location_dest_id': r['location_dest_id'],
-			                'origin': r['origin'], 'state': r['state'], 'partner_id': r['partner_id'],
-			                'sp_id': r['sp_id'], 'spo_id': r['spo_id'], 'picking_id': r['picking_id'], })
+		return res
 
+	@api.multi
+	def sort_and_create(self):
+		res = self.get_data()
+		data = {}
+		vals = {}
+		for r in res:
+			if r['partner_id'] in data:
+				data[r['partner_id']].append({'product_id': r['product_id'], 'product_qty':r['product_qty'],
+				                         'qty_done': r['qty_done'], 'name':r['name'],
+										 'location_dest_id': r['location_dest_id'],
+										 'origin': r['origin'], 'state': r['state'], 'partner_id': r['partner_id'],
+										 'sp_id': r['sp_id'], 'spo_id': r['spo_id'], 'picking_id': r['picking_id'],})
+			else:
+				data[r['partner_id']] = [{'product_id': r['product_id'], 'product_qty':r['product_qty'],
+				                         'qty_done': r['qty_done'], 'name':r['name'],
+										 'location_dest_id': r['location_dest_id'],
+										 'origin': r['origin'], 'state': r['state'], 'partner_id': r['partner_id'],
+										 'sp_id': r['sp_id'], 'spo_id': r['spo_id'], 'picking_id': r['picking_id'],}]
+
+		SPCL_obj = self.env['stock.picking.checking.line1']
+		SPC_obj = self.env['stock.picking.checking1']
+		lines = []
+		for d in data:
+			lines = [(0, 0, {'product_id': a['product_id'], 'product_qty': a['product_qty'],
+		                    'qty_done': a['qty_done'], 'name':a['name'],
+		                    'location_dest_id': a['location_dest_id'],
+		                    'origin': a['origin'], 'state': a['state'], 'partner_id': a['partner_id'],
+		                    'sp_id': a['sp_id'], 'spo_id': a['spo_id'], 'picking_id': a['picking_id'],
+			                'spc_line_id': 1})for a in data[d]]
+
+			vals.update({'partner_id': d, 'spc_line': lines})
+			SPC_obj.create(vals)
+			vals = {}
+			lines = []
+		# for r in res:
+		# 	SPCL_obj.create({'product_id': r['product_id'], 'product_qty':r['product_qty'],
+		# 	                'qty_done': r['qty_done'], 'name':r['name'],
+		# 	                'location_dest_id': r['location_dest_id'],
+		# 	                'origin': r['origin'], 'state': r['state'], 'partner_id': r['partner_id'],
+		# 	                'sp_id': r['sp_id'], 'spo_id': r['spo_id'], 'picking_id': r['picking_id'], })
+		# 	SPC_obj.create({'partner_id': r['partner_id'], 'spc_line_ids': lines})
+		# lines = [(0,0, {'product_id': r['product_id'], 'product_qty':r['product_qty'],
+		#                     'qty_done': r['qty_done'], 'name':r['name'],
+		#                      'location_dest_id': r['location_dest_id'],
+		#                      'origin': r['origin'], 'state': r['state'], 'partner_id': r['partner_id'],
+		#                      'sp_id': r['sp_id'], 'spo_id': r['spo_id'], 'picking_id': r['picking_id'], }) for r in res]
+		#
+		#
+		# 	# stock_picking_checking_search: id = 12798
+		search_view_id = self.env.ref('yamaha_inventory.stock_picking_checking_search1', False).id
+		#
 		return {
 				'name': 'Check picking',
-	            'view_type': 'tree',
-	            'view_mode': 'tree',
-	            'res_model': 'stock.picking.checking',
+	            'view_type': 'form',
+	            'view_mode': 'tree,form',
+				'src_model': 'stock.picking.checking1',
+	            'res_model': 'stock.picking.checking1',
 	            'type': 'ir.actions.act_window',
-	            'res_id': self.id,
-	            'target': 'current', }
+	            'target': 'current',
+				'id': self.id,
+				'context': {},
+			    'search_view_id': search_view_id,}
